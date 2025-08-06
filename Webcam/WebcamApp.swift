cf6306723e8bdee5b72defbcb48229f0250442d3
Maxim Golovlev <golovlev.maxim@gmail.com>
2025-08-06 11:05:19 +0300

    Initial Commit

---
+++ Webcam/WebcamApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebcamApp.swift
+//  Webcam
+//
+//  Created by Maxim Golovlev on 06.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct WebcamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
